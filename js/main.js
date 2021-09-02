0d46f16b5395925b2f94184d1e53abc7a51b2ae0
robertoAureli <r.aureli@protonmail.com>
2021-09-02 15:07:43 +0200

    Creo la select dinamica

--- js/main.js
+++ js/main.js
@@ -98,20 +98,66 @@ const icons = [{
 
 const containerCards = document.getElementById("icons");
 
+const search = document.getElementById("types");
+search.innerHTML = ` `;
+const types = getTypes(icons);
+types.forEach((type) => {
+        search.innerHTML += `
+             <option value="${type}">${type}</option>    
+        `
+})
+console.log(search);
+
+const colors = [
+    `blue`,
+    `orange`,
+    `purple`
+]
+
 containerCards.innerHTML = ` `;
 
-icons.forEach((icon) => {
+const coloredIcons = colorIcons(icons,colors);
+
+coloredIcons.forEach((icon) => {
     const {
         name,
         prefix,
         type,
-        family
+        family,
+        color
     } = icon;
     containerCards.innerHTML += `
     <div class="card">
-        <i class = "${family} ${prefix + name}" style="color: blue"></i>
-        <div class="title">${name}</div>
+        <i class = "${family} ${prefix + name}" style="color: ${color}"></i>
+        <div class="title">${name.toUpperCase()}</div>
     </div>
     `
 
 })
+
+function getTypes(array) {
+    const types = [];
+    array.forEach((element) => {
+        if (!types.includes(element.type)) {
+            types.push(element.type);
+        }
+    })
+    return types;
+}
+
+function colorIcons(array,colors) {
+    const types = getTypes(array);
+    console.log(types);
+    const coloredIcons = array.map((element)=>{
+        const indexTypes = types.indexOf(element.type);
+        element.color = colors[indexTypes];
+      
+        return element;
+
+    })
+ return coloredIcons;
+}
+
+// console.log(getTypes(icons));
+
+// console.log(colorIcons(icons,colors));
